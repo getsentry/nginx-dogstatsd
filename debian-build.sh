ff3b58f8c3b9be2fa0150a2eb5d0430f779c42ca
David Herberth <david.herberth@sentry.io>
2026-01-15 18:20:37 +0100

    fix(build): Updates the build scripts to use bookworm

--- debian-build.sh
+++ debian-build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+set -e
+
 apt-get update
 apt-get source nginx-light
 tar xvf nginx_*.debian.tar.xz
 
-echo -e "print-%:\n\t@echo" '$($*)' > printvar.mk
-configure_flags=$(make -f printvar.mk -f debian/rules print-light_configure_flags)
-
 cd nginx-*/
-./configure $(configure_flags) --add-dynamic-module=/opt/nginx-dogstatsd/
+./configure --add-dynamic-module=/opt/nginx-dogstatsd/
 make modules
 
-mv objs/ngx_http_dogstatsd_module.so /opt/nginx-dogstatsd/
+cp objs/ngx_http_dogstatsd_module.so /opt/nginx-dogstatsd/
